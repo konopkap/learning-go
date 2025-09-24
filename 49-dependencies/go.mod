2cf61be6c85e7909efa45915abaa9d2661c855b5
konopkap <konopka.patryk2@gmail.com>
2025-09-24 12:52:27 +0200

    Excercise 49

---
+++ 49-dependencies/go.mod
@@ -0,0 +1,5 @@
+module github.com/konopkap/learning-go/49-dependencies
+
+go 1.25.1
+
+require github.com/konopkap/learning-go-puppy v0.0.0-20250924103201-7fc8020922aa
---
+++ 49-dependencies/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/konopkap/learning-go-puppy"
+)
+
+func main() {
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+}
